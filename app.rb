525575d2e9f48e2096dad4f03bbfab31285161d6
NEHA SUWAl <neha@jyaasa.com>
2015-01-27 15:54:54 +0545

    post '/' do is modified.

--- app.rb
+++ app.rb
@@ -17,13 +17,9 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
-    # 1. Create an idea based on the form parameters
-    idea = Idea.new
-
-    # 2. Store it
+    idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
-
-    # 3. Send us back to the index page to see all ideas
     redirect '/'
   end
+  
 end
