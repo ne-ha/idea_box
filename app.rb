a4e484b3b3a8d24f0f66e30c1c8e871d07d1e2fc
NEHA SUWAl <neha@jyaasa.com>
2015-01-27 11:19:10 +0545

    this is the main file

---
+++ app.rb
@@ -0,0 +1,27 @@
+require './idea'
+require 'bundler'
+Bundler.require
+
+class IdeaBoxApp < Sinatra::Base
+   not_found do
+    erb :error
+  end
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+     erb :index, locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+    # # 1. Create an idea based on the form parameters
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    # # 2. Store it
+    idea.save
+    # # 3. Send us back to the index page to see all ideas
+    redirect '/'
+  end
+
+end
