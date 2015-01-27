8c9908c82737f764e53a5f995d2431e24b0e25e8
NEHA SUWAl <neha@jyaasa.com>
2015-01-27 13:50:56 +0545

    managing the indentation

--- idea.rb
+++ idea.rb
@@ -3,18 +3,9 @@ class Idea
   attr_reader :title, :description
 
   def initialize(attributes)
-  @title = attributes["title"]
-  @description = attributes["description"]
-end
-
-def save
-  database.transaction do
-    database['ideas'] ||= []
-    database['ideas'] << {"title" => title, "description" => description}
+    @title = attributes["title"]
+    @description = attributes["description"]
   end
-end
-
-
 
   def self.all
     raw_ideas.map do |data|
